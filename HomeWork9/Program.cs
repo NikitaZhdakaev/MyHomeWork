7ce8d90017846043f7fb7458426b40e107872b9a
Nikita Zhdakaev <n-5@inbox.ru>
2022-07-07 19:48:45 +0500

    Домашнее задание №9, задача 67

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -34,3 +34,15 @@ int InputNumbers(string input)
 }
 */
 
+/* Задача 67: Напишите программу, 
+которая будет принимать на вход число и возвращать кол-во его цифр.
+
+int SumOfDigits(int number)
+    {
+ if(number/10==0) return 1;
+ return 1 + SumOfDigits(number/10);
+    }
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfDigits(a));
+*/
